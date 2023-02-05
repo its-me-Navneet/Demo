3df674f51ccdc3e81377c74920ffcf47d2632682
Navneet <kumarnavneetkumar14@gmail.com>
2023-02-05 17:05:10 +0530

    Template

---
+++ Template.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int 
+#define forr(n) for(int i=0;i<n;i++) 
+#define vec vector<int> 
+#define pb push_back 
+#define bg begin()
+#define en end()
+#define mod 1000000007   
+#define all(v) v.begin(),v.end()
+typedef tuple<string,int,int> tp;  
+
+
+
+void solve(){
+   
+ 
+  
+
+}
+
+int32_t main(){ 
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
+     cout.tie(NULL);
+    int t=1; 
+    cin>>t;
+    while(t--){ 
+        solve();
+    }
+    
+    return 0;
+}
